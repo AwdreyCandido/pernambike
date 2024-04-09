d3320c539ce8905cf1c0152450bbb2a9e3c6418c
AwdreyCandido <awdrey.dev@gmail.com>
2024-04-09 04:54:51 -0300

    feat: configuring custom fonts and splash screen

--- App.tsx
+++ App.tsx
@@ -1,22 +1,48 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { useCallback, useEffect, useState } from "react";
+import Entypo from "@expo/vector-icons/Entypo";
+import { appFonts } from "./src/utils/fonts";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  useFonts({
-    "sora regular": require("./assets/fonts/Sora-Regular.ttf"),
-    "sora medium": require("./assets/fonts/Sora-Medium.ttf"),
-    "sora semibold": require("./assets/fonts/Sora-SemiBold.ttf"),
-    "sora bold": require("./assets/fonts/Sora-Bold.ttf"),
-    "dmsans regular": require("./assets/fonts/DMSans-Regular.ttf"),
-    "dmsans medium": require("./assets/fonts/DMSans-Medium.ttf"),
-    "dmsans semibold": require("./assets/fonts/DMSans-SemiBold.ttf"),
-    "dmsans bold": require("./assets/fonts/DMSans-Bold.ttf"),
-  });
+  const [appIsReady, setAppIsReady] = useState(false);
+  useFonts(appFonts);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync(Entypo.font);
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
 
   return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: "sora bold", fontSize: 20 }}>Pernambike</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontFamily: "sora regular", fontSize: 20 }}>
+        Pernambike
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/utils/fonts.ts
@@ -0,0 +1,10 @@
+export const appFonts = {
+  "sora regular": require("./../../assets/fonts/Sora-Regular.ttf"),
+  "sora medium": require("./../../assets/fonts/Sora-Medium.ttf"),
+  "sora semibold": require("./../../assets/fonts/Sora-SemiBold.ttf"),
+  "sora bold": require("./../../assets/fonts/Sora-Bold.ttf"),
+  "dmsans regular": require("./../../assets/fonts/DMSans-Regular.ttf"),
+  "dmsans medium": require("./../../assets/fonts/DMSans-Medium.ttf"),
+  "dmsans semibold": require("./../../assets/fonts/DMSans-SemiBold.ttf"),
+  "dmsans bold": require("./../../assets/fonts/DMSans-Bold.ttf"),
+};
